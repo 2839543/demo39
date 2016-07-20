8f66b4e3a4881d9b5ecb2fd2869524cc9bd3b523
hao richie <hao richie>
2016-07-20 22:41:32 +0800

    first commit

---
+++ Weibo/Home/Controller/UserController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richie
+ * Date: 2016/7/20
+ * Time: 22:26
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class UserController extends Controller
+{
+    public function  index(){
+        echo 'user ';
+    }
+    public function  test (){
+        echo 'user test';
+    }
+}
